5073c8d928aeba4476264df8f92866c8eb0d926b
Nilber1124 <apnilber@gmail.com>
2026-09-09 15:18:19 -0500

    Creacion de proyecto ISANORTE

---
+++ src/main/java/com/isanorte/constructora_api/ConstructoraApiApplication.java
@@ -0,0 +1,13 @@
+package com.isanorte.constructora_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConstructoraApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConstructoraApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/isanorte/constructora_api/ConstructoraApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isanorte.constructora_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConstructoraApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
